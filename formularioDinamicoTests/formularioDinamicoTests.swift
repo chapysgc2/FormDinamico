4db5c1e4c933e1469dc3699c6b41b56200536c31
hazelgc2 <hazelo235@gmail.com>
2024-10-12 23:19:20 -0600

    Initial Commit

---
+++ formularioDinamicoTests/formularioDinamicoTests.swift
@@ -0,0 +1,17 @@
+//
+//  formularioDinamicoTests.swift
+//  formularioDinamicoTests
+//
+//  Created by Hazel Alain on 12/10/24.
+//
+
+import Testing
+@testable import formularioDinamico
+
+struct formularioDinamicoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
